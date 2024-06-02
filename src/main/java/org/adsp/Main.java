9a507299f52b0b4498e6842a8a8fd8614ce6bd12
Oscar Bautista <oscar.adba@gmail.com>
2024-06-02 15:29:40 -0500

    Prototype Shadow copy

--- src/main/java/org/adsp/Main.java
+++ src/main/java/org/adsp/Main.java
@@ -1,17 +1,23 @@
 package org.adsp;
 
-import org.adsp.gui.FrameUI;
-
-import javax.swing.*;
-import java.awt.*;
+import org.adsp.patterns.creational.Prototype.Person;
 
 public class Main {
     public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            JFrame frame = new FrameUI();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(1050, 550);
-            frame.setVisible(true);
-        });
+        // Original Object
+        Person p = new Person("Person-A", "Civic");
+        System.out.println("Original (original values): " + p.getName() + " - " + p.getCar().getName());
+
+        // Clone as a shallow copy
+        Person q = (Person) p.clone();
+        System.out.println("Clone (before change): " + q.getName() + " - " + q.getCar().getName());
+
+        // Change the primitive member
+        q.setName("Person-B");
+
+        // Change the lower-level object
+        q.getCar().setName("Accord");
+        System.out.println("Clone (after change): " + q.getName() + " - " + q.getCar().getName());
+        System.out.println("Original (after clone is modified): " + p.getName() + " - " + p.getCar().getName());
     }
 }
---
+++ src/main/java/org/adsp/patterns/creational/Prototype/Person.java
@@ -0,0 +1,32 @@
+package org.adsp.patterns.creational.Prototype;
+
+public class Person implements Cloneable {
+    //Lower-level object
+    private final Car car;
+    private String name;
+
+    public Car getCar() {
+        return car;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String s) {
+        name = s;
+    }
+
+    public Person(String s, String t) {
+        name = s;
+        car = new Car(t);
+    }
+
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
+}
